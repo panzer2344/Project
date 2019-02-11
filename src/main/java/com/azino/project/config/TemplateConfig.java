f2714805e7852dafbe6121ccdf350d09f0632480
panzer2344 <lexathebest23@gmail.com>
2019-02-11 22:31:04 +0300

    Added thymeleaf-exstras-springsecurity. Started adding delete button to items on index page only for users with admin role.

---
+++ src/main/java/com/azino/project/config/TemplateConfig.java
@@ -0,0 +1,27 @@
+package com.azino.project.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class TemplateConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver, SpringSecurityDialect sec) {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.addDialect(sec); // Enable use of "sec"
+        return templateEngine;
+    }
+}
--- src/main/java/com/azino/project/config/WebSecurityConfig.java
+++ src/main/java/com/azino/project/config/WebSecurityConfig.java
@@ -70,7 +70,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         // The pages does not require login
-        http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/logout").permitAll();
 
         // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
         // If no login, it will redirect to /login page.
